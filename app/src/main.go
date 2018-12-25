b1d9fe88ce7cd2543468474f7505f47cf427c061
Herman Mak <6619094+hermanma@users.noreply.github.com>
2018-12-25 18:34:21 +0800

    remove unused handler

--- app/src/main.go
+++ app/src/main.go
@@ -6,13 +6,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", index)
 	http.HandleFunc("/service", handle)
 	http.ListenAndServe(":7000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
